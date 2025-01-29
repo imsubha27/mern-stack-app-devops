1d7860a86043d70e4d95ba0e69e269dc4956d422
imsubha27 <imsubha27@gmail.com>
2025-01-29 19:57:47 +0530

    initial commit

---
+++ install.sh/install_awscli.sh
@@ -0,0 +1,6 @@
+#AWS CLI installation script
+
+#!/bin/bash
+curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
---
+++ install.sh/install_java.sh
@@ -0,0 +1,5 @@
+#Java installation script
+
+#!/bin/bash
+sudo apt install openjdk-17-jdk -y
+java --version
