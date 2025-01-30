a81985de39ef370c0290835e10f20f1bcf1f8485
imsubha27 <imsubha27@gmail.com>
2025-01-30 09:53:15 +0530

    update commit

--- install.sh/install_awscli.sh
+++ install.sh/install_awscli.sh
@@ -1,6 +1,7 @@
 #AWS CLI installation script
 
 #!/bin/bash
+sudo apt install -y wget unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
--- install.sh/install_java.sh
+++ install.sh/install_java.sh
@@ -1,5 +1,7 @@
 #Java installation script
 
 #!/bin/bash
+sudo apt update -y
+udo apt install openjdk-17-jre -y
 sudo apt install openjdk-17-jdk -y
 java --version
